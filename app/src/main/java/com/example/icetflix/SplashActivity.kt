18841f6e1a70e80f006d1915f5d5ddf8b7bcc3d0
CarlosBitizin <carlospintorossy07@gmail.com>
2022-06-26 17:42:41 -0400

    login screen layout

--- app/src/main/java/com/example/icetflix/SplashActivity.kt
+++ app/src/main/java/com/example/icetflix/SplashActivity.kt
@@ -15,7 +15,7 @@ class SplashActivity : AppCompatActivity() {
 
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
+            val intent = Intent(this,SingInActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
